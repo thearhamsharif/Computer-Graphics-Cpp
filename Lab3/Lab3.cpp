6c38db87dec472076cf4943c0c6c83b81c0ef6bc
Arham Sharif <sarham928@gmail.com>
2023-10-31 17:15:11 +0500

    1

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -44,8 +44,6 @@ using namespace std;
 //    glLoadIdentity();
 //    gluOrtho2D(0.0,(GLdouble)screenwidth,0.0,(GLdouble)screenHeight);
 //   
-////    glMatrixMode(GL_MODELVIEW);
-////    glLoadIdentity();
 //}
 //void mouseMove(int x , int y)
 //{
@@ -78,52 +76,49 @@ using namespace std;
 
 //02
 
-void myinit(void)
-{
-    //glClearColor(1.0, 1.0, 1.0, 0.0); // Set background color to white
-    glClearColor(0.0, 0.0, 0.0, 1.0); // Set the clear color to black
-    glColor3f(0.0, 0.0, 1.0); // Set the drawing color to blue
-    glPointSize(10.0);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(0, 640, 0, 480);
-}
-
-void display(void)
-{   
-    glFlush();
-}
-
-void mouse(int button, int state, int x, int y)
-{   
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-        
-        glBegin(GL_POINTS);
-        glVertex2d(x, 480 - y);
-        glEnd();
-        
-    }
-    //glFlush(); 
-    else if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
-        //glClearColor(1.0, 1.0, 1.0, 0.0); // Reset background to white
-        glClearColor(0.0, 0.0, 0.0, 1.0); // Set the clear color to black
-        glClear(GL_COLOR_BUFFER_BIT);   
-    }
-    glutPostRedisplay();
-}
-
-int main(int argc, char** argv)
-{
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(640, 480);
-    glutCreateWindow("Arham Sharif EB21102022");
-    glutDisplayFunc(display);
-    glutMouseFunc(mouse);
-    myinit();
-    glutMainLoop();
-    return 0;
-}
+//void myinit(void)
+//{
+//    glClearColor(0.0, 0.0, 0.0, 1.0); // Set the clear color to black
+//    glColor3f(0.0, 0.0, 1.0); // Set the drawing color to blue
+//    glPointSize(10.0);
+//    glMatrixMode(GL_PROJECTION);
+//    glLoadIdentity();
+//    gluOrtho2D(0, 640, 0, 480);
+//}
+//
+//void display(void)
+//{   
+//    glFlush();
+//}
+//
+//void mouse(int button, int state, int x, int y)
+//{   
+//    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+//        
+//        glBegin(GL_POINTS);
+//        glVertex2d(x, 480 - y);
+//        glEnd();
+//        
+//    }
+//    else if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+//        glClearColor(0.0, 0.0, 0.0, 1.0); // Set the clear color to black
+//        glClear(GL_COLOR_BUFFER_BIT);   
+//    }
+//    glutPostRedisplay();
+//}
+//
+//int main(int argc, char** argv)
+//{
+//    glutInit(&argc, argv);
+//    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+//    glutInitWindowSize(640, 480);
+//    glutCreateWindow("Arham Sharif EB21102022");
+//    glutDisplayFunc(display);
+//    glutMouseFunc(mouse);
+//    myinit();
+//    glutMainLoop();
+//    return 0;
+//}
  
  
 // 03
