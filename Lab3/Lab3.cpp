10de745b149573bef99435ce5d0a7ec2f5074b8f
Arham Sharif <sarham928@gmail.com>
2023-10-31 17:20:11 +0500

    Lab 3 - 3

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -122,46 +122,45 @@ using namespace std;
  
  
 // 03
-//void myinit(void)
-//{
-//    //glClearColor(1.0, 1.0, 1.0, 0.0); // Set background color to white
-//    glClearColor(0.0, 0.0, 0.0, 1.0); // Set the clear color to black
-//    glColor3f(0.0, 0.0, 1.0); // Set the drawing color to blue
-//    gluOrtho2D(0, 640, 0, 480);
-//}
-//
-//void display(void)
-//{   
-//    //glClear(GL_COLOR_BUFFER_BIT);
-//    glFlush();
-//}
-//
-//void myMovedMouse(int mouseX, int mouseY)
-//{
-//	
-//  int x = mouseX; //grab the mouse position
-//  int y = 480 - mouseY; // flip it as usual
-//  int brushSize = 10;
-//  glRecti(x,y, x + brushSize, y + brushSize);
-//  glFlush();
-//  
-//  
-//}
-//
-//
-//int main(int argc, char** argv)
-//{
-//    glutInit(&argc, argv);
-//    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-//    glutInitWindowSize(640, 480);
-//    glutCreateWindow("Point Creation");
-//    glutMotionFunc(myMovedMouse);
-//    glutDisplayFunc(display);
-//    
-//    myinit();
-//    glutMainLoop();
-//    return 0;
-//}
+
+void myinit(void)
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0); // Set the clear color to black
+    glColor3f(0.0, 0.0, 1.0); // Set the drawing color to blue
+    gluOrtho2D(0, 640, 0, 480);
+}
+
+void display(void)
+{   
+    glFlush();
+}
+
+void myMovedMouse(int mouseX, int mouseY)
+{
+	
+  int x = mouseX; //grab the mouse position
+  int y = 480 - mouseY; // flip it as usual
+  int brushSize = 10;
+  glRecti(x,y, x + brushSize, y + brushSize);
+  glFlush();
+  
+  
+}
+
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(640, 480);
+    glutCreateWindow("Arham Sharif EB21102022");
+    glutMotionFunc(myMovedMouse);
+    glutDisplayFunc(display);
+    
+    myinit();
+    glutMainLoop();
+    return 0;
+}
 
 //-------------------------------------------------------
 //04 
