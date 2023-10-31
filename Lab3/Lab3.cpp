532c2fdb0f3fb566620a7e98c8fbac5b3cc08b06
Arham Sharif <sarham928@gmail.com>
2023-10-31 17:30:12 +0500

    Lab 3 - 4 not completed yet

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -123,58 +123,55 @@ using namespace std;
  
 // 03
 
-void myinit(void)
-{
-    glClearColor(0.0, 0.0, 0.0, 1.0); // Set the clear color to black
-    glColor3f(0.0, 0.0, 1.0); // Set the drawing color to blue
-    gluOrtho2D(0, 640, 0, 480);
-}
-
-void display(void)
-{   
-    glFlush();
-}
-
-void myMovedMouse(int mouseX, int mouseY)
-{
-	
-  int x = mouseX; //grab the mouse position
-  int y = 480 - mouseY; // flip it as usual
-  int brushSize = 10;
-  glRecti(x,y, x + brushSize, y + brushSize);
-  glFlush();
-  
-  
-}
-
-
-int main(int argc, char** argv)
-{
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(640, 480);
-    glutCreateWindow("Arham Sharif EB21102022");
-    glutMotionFunc(myMovedMouse);
-    glutDisplayFunc(display);
-    
-    myinit();
-    glutMainLoop();
-    return 0;
-}
-
-//-------------------------------------------------------
-//04 
-//#include<windows.h> 
-//#include"mmsystem.h"
-//int main()
+//void myinit(void)
 //{
-//	PlaySound(TEXT("Rains Of Castamere Instrumental.wav"),NULL,SND_SYNC);
-//	/*TEXT("Rains Of Castamere Instrumental.wav"): This is the first argument to the PlaySound function and specifies the name of the audio file to be played. The TEXT macro is used to make the string compatible with both Unicode and ANSI character encodings, depending on the build configuration of the program.
-//    NULL: This is the second argument, and it represents a handle to a module (in this case, NULL indicates the current module). It's not used in this context and can be set to NULL.
-//   SND_SYNC: This is the third argument and is a flag that specifies how the sound should be played. SND_SYNC indicates that the sound should be played synchronously, which means the PlaySound function will not return until the sound has finished playing.
-//    */
-//	return 0;
+//    glClearColor(0.0, 0.0, 0.0, 1.0); // Set the clear color to black
+//    glColor3f(0.0, 0.0, 1.0); // Set the drawing color to blue
+//    gluOrtho2D(0, 640, 0, 480);
+//}
+//
+//void display(void)
+//{   
+//    glFlush();
+//}
+//
+//void myMovedMouse(int mouseX, int mouseY)
+//{
+//	
+//  int x = mouseX; //grab the mouse position
+//  int y = 480 - mouseY; // flip it as usual
+//  int brushSize = 10;
+//  glRecti(x,y, x + brushSize, y + brushSize);
+//  glFlush();
+//  
+//  
+//}
+//
+//
+//int main(int argc, char** argv)
+//{
+//    glutInit(&argc, argv);
+//    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+//    glutInitWindowSize(640, 480);
+//    glutCreateWindow("Arham Sharif EB21102022");
+//    glutMotionFunc(myMovedMouse);
+//    glutDisplayFunc(display);
+//    
+//    myinit();
+//    glutMainLoop();
+//    return 0;
 //}
 
+//04 
 
-
+#include<windows.h> 
+#include"mmsystem.h"
+int main()
+{
+	PlaySound(TEXT("Rains Of Castamere Instrumental.wav"),NULL,SND_SYNC);
+	/*TEXT("Rains Of Castamere Instrumental.wav"): This is the first argument to the PlaySound function and specifies the name of the audio file to be played. The TEXT macro is used to make the string compatible with both Unicode and ANSI character encodings, depending on the build configuration of the program.
+    NULL: This is the second argument, and it represents a handle to a module (in this case, NULL indicates the current module). It's not used in this context and can be set to NULL.
+   SND_SYNC: This is the third argument and is a flag that specifies how the sound should be played. SND_SYNC indicates that the sound should be played synchronously, which means the PlaySound function will not return until the sound has finished playing.
+    */
+	return 0;
+}
