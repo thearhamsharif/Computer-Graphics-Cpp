76f8a0f397303bc263c54bc8d929519783aa8a8c
Arham Sharif <sarham928@gmail.com>
2023-11-02 18:39:30 +0500

    Lab 3 (1, 2, 3, 5)

--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 //01
 
-//int screenwidth = 640;
+//int screenWidth = 640;
 //int screenHeight = 480;
 //
 //void myDisplay(void)
@@ -38,11 +38,14 @@ using namespace std;
 //void myReshape(int w ,int h)
 //{
 //	int screenwidth = w;
-//    int screenHeight = h;
+// 	int screenHeight = h;
 //   
 //    glMatrixMode(GL_PROJECTION);
 //    glLoadIdentity();
 //    gluOrtho2D(0.0,(GLdouble)screenwidth,0.0,(GLdouble)screenHeight);
+//    
+////    glMatrixModel(GL_MODELVIEW)
+////    glLoadIdentity();
 //   
 //}
 //void mouseMove(int x , int y)
@@ -62,7 +65,7 @@ using namespace std;
 //int main(int argc, char** argv) {
 //    glutInit(&argc, argv);
 //    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-//    glutInitWindowSize(500, 500);
+//    glutInitWindowSize(screenWidth, screenHeight);
 //    glutCreateWindow("Arham Sharif EB21102022");  
 //    myinit();
 //    glutReshapeFunc(myReshape);
@@ -76,49 +79,49 @@ using namespace std;
 
 //02
 
-//void myinit(void)
-//{
-//    glClearColor(0.0, 0.0, 0.0, 1.0); // Set the clear color to black
-//    glColor3f(0.0, 0.0, 1.0); // Set the drawing color to blue
-//    glPointSize(10.0);
-//    glMatrixMode(GL_PROJECTION);
-//    glLoadIdentity();
-//    gluOrtho2D(0, 640, 0, 480);
-//}
-//
-//void display(void)
-//{   
-//    glFlush();
-//}
-//
-//void mouse(int button, int state, int x, int y)
-//{   
-//    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-//        
-//        glBegin(GL_POINTS);
-//        glVertex2d(x, 480 - y);
-//        glEnd();
-//        
-//    }
-//    else if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
-//        glClearColor(0.0, 0.0, 0.0, 1.0); // Set the clear color to black
-//        glClear(GL_COLOR_BUFFER_BIT);   
-//    }
-//    glutPostRedisplay();
-//}
-//
-//int main(int argc, char** argv)
-//{
-//    glutInit(&argc, argv);
-//    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-//    glutInitWindowSize(640, 480);
-//    glutCreateWindow("Arham Sharif EB21102022");
-//    glutDisplayFunc(display);
-//    glutMouseFunc(mouse);
-//    myinit();
-//    glutMainLoop();
-//    return 0;
-//}
+void myinit(void)
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0); // Set the clear color to black
+    glColor3f(0.0, 0.0, 1.0); // Set the drawing color to blue
+    glPointSize(10.0);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, 640, 0, 480);
+}
+
+void display(void)
+{   
+    glFlush();
+}
+
+void mouse(int button, int state, int x, int y)
+{   
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        
+        glBegin(GL_POINTS);
+        glVertex2d(x, 480 - y);
+        glEnd();
+        
+    }
+    else if(button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+        glClearColor(0.0, 0.0, 0.0, 1.0); // Set the clear color to black
+        glClear(GL_COLOR_BUFFER_BIT);   
+    }
+    glutPostRedisplay();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(640, 480);
+    glutCreateWindow("Arham Sharif EB21102022");
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    myinit();
+    glutMainLoop();
+    return 0;
+}
  
  
 // 03
@@ -164,14 +167,14 @@ using namespace std;
 
 //04 
 
-#include<windows.h> 
-#include"mmsystem.h"
-int main()
-{
-	PlaySound(TEXT("Rains Of Castamere Instrumental.wav"),NULL,SND_SYNC);
-	/*TEXT("Rains Of Castamere Instrumental.wav"): This is the first argument to the PlaySound function and specifies the name of the audio file to be played. The TEXT macro is used to make the string compatible with both Unicode and ANSI character encodings, depending on the build configuration of the program.
-    NULL: This is the second argument, and it represents a handle to a module (in this case, NULL indicates the current module). It's not used in this context and can be set to NULL.
-   SND_SYNC: This is the third argument and is a flag that specifies how the sound should be played. SND_SYNC indicates that the sound should be played synchronously, which means the PlaySound function will not return until the sound has finished playing.
-    */
-	return 0;
-}
+//#include<windows.h> 
+//#include"mmsystem.h"
+//int main()
+//{
+//	PlaySound(TEXT("Rains Of Castamere Instrumental.wav"),NULL,SND_SYNC);
+//	/*TEXT("Rains Of Castamere Instrumental.wav"): This is the first argument to the PlaySound function and specifies the name of the audio file to be played. The TEXT macro is used to make the string compatible with both Unicode and ANSI character encodings, depending on the build configuration of the program.
+//    NULL: This is the second argument, and it represents a handle to a module (in this case, NULL indicates the current module). It's not used in this context and can be set to NULL.
+//   SND_SYNC: This is the third argument and is a flag that specifies how the sound should be played. SND_SYNC indicates that the sound should be played synchronously, which means the PlaySound function will not return until the sound has finished playing.
+//    */
+//	return 0;
+//}
